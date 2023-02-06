5b9a5f7617e2aeec18072845258432c0a7055841
QuantMad <quantmad@gmail.com>
2023-02-06 08:57:35 -0500

    Edit build script

--- build_arm64_deb.sh
+++ build_arm64_deb.sh
@@ -5,8 +5,10 @@ chmod +x ./airalert_1.0-1_arm64/DEBIAN/postinst
 cp ./postrm ./airalert_1.0-1_arm64/DEBIAN/postrm
 chmod +x ./airalert_1.0-1_arm64/DEBIAN/postrm
 
+mkdir -p ./airalert_1.0-1_arm64/etc/systemd/system
 cp ./airalertd.service ./airalert_1.0-1_arm64/etc/systemd/system/airalertd.service
 
+mkdir -p ./airalert_1.0-1_arm64/usr/local/bin/airalert/
 cp -r ./res ./airalert_1.0-1_arm64/usr/local/bin/airalert/res
 
 env GOOS=linux GOARCH=arm64 go build -o ./airalert_1.0-1_arm64/usr/local/bin/airalert/airalert
