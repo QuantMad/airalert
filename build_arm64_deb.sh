a49a039d4442b0e8049c1f8f983d4072b3b64022
QuantMad <quantmad@gmail.com>
2023-02-06 07:59:11 -0500

    Added simple build script for arm64

---
+++ build_arm64_deb.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+cp ./postinst ./airalert_1.0-1_arm64/DEBIAN/postinst
+chmod +x ./airalert_1.0-1_arm64/DEBIAN/postinst
+cp ./postrm ./airalert_1.0-1_arm64/DEBIAN/postrm
+chmod +x ./airalert_1.0-1_arm64/DEBIAN/postrm
+
+cp ./airalertd.service ./airalert_1.0-1_arm64/etc/systemd/system/airalertd.service
+
+cp -r ./res ./airalert_1.0-1_arm64/usr/local/bin/airalert/res
+
+env GOOS=linux GOARCH=arm64 go build -o ./airalert_1.0-1_arm64/usr/local/bin/airalert/airalert
+dpkg-deb --build --root-owner-group airalert_1.0-1_arm64
+mv ./airalert_1.0-1_arm64.deb ./builds/airalert_1.0-1_arm64.deb
